9e3f563d39408c0d6a15e58bf79da2f0aeadb80c
Yoshihiro Fujita <yoshixxx009@gmail.com>
2020-03-06 13:03:59 +0000

    課題提出

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   validates :name, presence:true,length: {maximum: 15}
-  validates :email,presence:true
-  validates :password, presence: true, length: { minimum: 8, maximum: 32}
-VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/
-VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
-
-
-has_secure_password
+  
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
+  validates :email, presence:true, format: { with: VALID_EMAIL_REGEX } 
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
+  
+  has_secure_password
 end
  
