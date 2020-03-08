cd7efea6395b039e6d3fdaafd01b7619edd75df7
Yoshihiro Fujita <yoshixxx009@gmail.com>
2020-03-08 09:10:59 +0000

    課題を提出します

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get "pages/help"
   
   resources :users
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
